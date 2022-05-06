a33ee99c1cb76189ad993354df2fc8c150806f1c
m47m <xm130812@163.com>
2022-05-06 13:31:56 +0800

    init

---
+++ src/views/Disaster/disaster-show.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+         <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-calendar"></i> 首页
+                </el-breadcrumb-item>
+                <el-breadcrumb-item>灾情显示</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+         <!--在此写数据，非必要不动其他文件-->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "donate"
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Disaster/disaster-visual.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+         <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-calendar"></i> 首页
+                </el-breadcrumb-item>
+                <el-breadcrumb-item>数据可视化</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+         
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "donate"
+};
+</script>
+
+<style>
+</style>
